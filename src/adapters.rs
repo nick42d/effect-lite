3ca05264752e11fc6e192248e22b789fe960253b
Nick Dowsett <nickdowsett42@gmail.com>
2026-03-29 20:56:53 +0800

    Migrate from Stream dependent version

--- src/adapters.rs
+++ src/adapters.rs
@@ -2,10 +2,39 @@ use crate::Effect;
 use futures::{future::Either, stream::select, Stream, StreamExt};
 
 pub trait EffectExt<D>: Effect<D> {
+    /// Map the output of an effect.
+    /// ```
+    /// let x = Once("Hello, world!");
+    /// let y = MapOutput {
+    ///     effect: x,
+    ///     map_fn: |s| s.to_ascii_lowercase()
+    /// };
+    /// assert_eq!(y.into_stream(()).next().await, "hello, world!");
+    /// ```
     fn map_output<T, F>(self, map_fn: F) -> MapOutput<Self, F>
     where
         Self: Sized,
         F: FnMut(Self::Output) -> T,
+    {
+        MapOutput {
+            effect: self,
+            map_fn,
+        }
+    }
+    /// Map the output of an effect (async closure support)
+    /// ```
+    /// let x = Once("Hello, world!");
+    /// let y = MapOutput {
+    ///     effect: x,
+    ///     map_fn: |s| s.to_ascii_lowercase()
+    /// };
+    /// assert_eq!(y.into_stream(()).next().await, "hello, world!");
+    /// ```
+    fn map_output_async<T, F, Fut>(self, map_fn: F) -> MapOutput<Self, F>
+    where
+        Self: Sized,
+        F: Fn(Self::Output) -> Fut,
+        Fut: Future<Output = T>,
     {
         MapOutput {
             effect: self,
@@ -22,61 +51,23 @@ pub trait EffectExt<D>: Effect<D> {
             map_fn,
         }
     }
-    /// Map the output and dependency of an Effect.
-    /// ```
-    /// let x = "Hello, world";
-    /// let y = once_dependency(&x).map_output(|_| "world");
-    /// let z = y.map(|dependency: &str, output: &str| dependency.find(frontend);
-    /// assert_eq!(y.into_stream(()).next().await, "hello, world!");
-    /// ```
-    fn map<T, F>(self, map_fn: F) -> Map<Self, F>
-    where
-        Self: Sized,
-        F: FnOnce(D, Self::Output) -> T,
-    {
-        Map {
-            effect: self,
-            map_fn,
-        }
-    }
-    fn select_either<E2, D2>(self, other: E2) -> SelectEither<Self, E2>
+    /// Merge two effects.
+    /// # Resolution note
+    /// The first effect is resolved first.
+    // TODO: how to swap resolution order...
+    fn merge<E2, D2>(self, other: E2) -> Merge<Self, E2>
     where
         Self: Sized,
         E2: Effect<D2>,
     {
-        SelectEither {
+        Merge {
             effect_1: self,
             effect_2: other,
         }
     }
 }
-pub struct Map<E, F> {
-    effect: E,
-    map_fn: F,
-}
-
-// impl<D, E, F, T> Effect<D> for Map<E, F>
-// where
-//     E: Effect<D>,
-//     F: FnMut(D, E::Output) -> T,
-// {
-//     type Output = T;
-//     // TODO
-//     fn into_stream(self, dependency: D) -> impl Stream<Item = Self::Output> {
-//         let Self { effect, map_fn } = self;
-//         effect.into_stream(dependency).map(map_fn)
-//     }
-// }
 
 /// Map the output of an Effect.
-/// ```
-/// let x = Once("Hello, world!");
-/// let y = MapOutput {
-///     effect: x,
-///     map_fn: |s| s.to_ascii_lowercase()
-/// };
-/// assert_eq!(y.into_stream(()).next().await, "hello, world!");
-/// ```
 pub struct MapOutput<E, F> {
     effect: E,
     map_fn: F,
@@ -85,12 +76,12 @@ pub struct MapOutput<E, F> {
 impl<D, E, F, T> Effect<D> for MapOutput<E, F>
 where
     E: Effect<D>,
-    F: FnMut(E::Output) -> T,
+    F: Fn(E::Output) -> T,
 {
     type Output = T;
-    fn into_stream(self, dependency: D) -> impl Stream<Item = Self::Output> {
+    fn resolve(self, dependency: D) -> Self::Output {
         let Self { effect, map_fn } = self;
-        effect.into_stream(dependency).map(map_fn)
+        map_fn(effect.resolve(dependency))
     }
 }
 
@@ -105,29 +96,26 @@ where
     F: FnOnce(D2) -> D1,
 {
     type Output = E::Output;
-    fn into_stream(self, dependency: D2) -> impl Stream<Item = Self::Output> {
+    fn resolve(self, dependency: D2) -> Self::Output {
         let Self { effect, map_fn } = self;
-        effect.into_stream(map_fn(dependency))
+        effect.resolve(map_fn(dependency))
     }
 }
 
-pub struct SelectEither<E1, E2> {
+pub struct Merge<E1, E2> {
     effect_1: E1,
     effect_2: E2,
 }
 
-impl<D1, D2, E1, E2> Effect<(D1, D2)> for SelectEither<E1, E2>
+impl<D1, D2, E1, E2> Effect<(D1, D2)> for Merge<E1, E2>
 where
     E1: Effect<D1>,
     E2: Effect<D2>,
 {
-    type Output = Either<E1::Output, E2::Output>;
-    fn into_stream(self, dependency: (D1, D2)) -> impl Stream<Item = Self::Output> {
+    type Output = (E1::Output, E2::Output);
+    fn resolve(self, dependency: (D1, D2)) -> Self::Output {
         let Self { effect_1, effect_2 } = self;
         let (d1, d2) = dependency;
-        select(
-            effect_1.into_stream(d1).map(Either::Left),
-            effect_2.into_stream(d2).map(Either::Right),
-        )
+        (effect_1.resolve(d1), effect_2.resolve(d2))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,39 @@
 #![no_std]
-//! # EffectX
+//! # effect-lite
+//! Lightweight (no-std compatible), opinionated algebraic effects library for Rust.
+//! ## Goals
+//! - No-std compatibility (feature gated).
+//! - Demistify the benefits of algebraic effects for those new to functional-style programming.
+//! ## Non-goals
+//! - Incorporate advanced runtimes or application frameworks - this should be a low-level crate with higher level features built into dependent crates.
+//! - Use traditional FP method names at the expense of onboarding difficulty - this is an opinionated crate and I am not a purist.
 //! ## Examples
 //! ### Dependency injection
-//! // An effect that requires access to a `reqwest::Client` to run.
+//! // An effect that requires access to the inaccessible application state in the parent struct.
 //! ```
-//! let client = reqwest::Client::new();
-//! let effect = once_dependency(&client);
-//! effect.map_output(|client|
-//!     client.get("https://www.google.com").send().await.unwrap().text.await.unwrap()
-//! );
-//! let output = runtime::to_iter(&client, effect).next().unwrap();
-//! println!("{output}");
+//! #[cfg(Default)]
+//! struct AppState {
+//!     count: usize,
+//!     component: component::Component,
+//! }
+//! #[cfg(Default)]
+//! mod component {
+//!     pub struct Component {
+//!         pub message: String
+//!     }
+//!     impl Component {
+//!         pub fn handle_message(&mut self, msg: String) -> impl Effect<()> {
+//!             self.message = msg;
+//!             once_dependency::<&AppState>().map_output(|app_state| app_state.counter +=1)
+//!         }
+//!     }
+//! }
+//! let state = AppState::default();
+//! let effect = state.component.handle_message("Hello, world");
+//! assert_eq!(&state.component.message, "Hello, world!");
+//! assert_eq!(&state.count, 0);
+//! effect.resolve(&mut state);
+//! assert_eq!(&state.count, 1);
 //! ```
 //! ### Centralise side effects
 //! ```
@@ -21,22 +44,64 @@
 //!     }
 //! }
 //! let printer = StdoutPrinter;
-//! let effect = once_dependency(&printer).map_output(|printer| printer.print("Hello, world"));
+//! let effect = once_dependency::<&StdoutPrinter>()
+//!     .map_output(|printer| printer.print("Hello, world"));
 //! // Prints to console here.
-//! runtime::to_iter(&printer, effect).next().unwrap();
+//! effect.resolve(&StdoutPrinter);
 //! ```
-
-use futures::{
-    future::Either,
-    stream::{self, select},
-    Stream, StreamExt,
-};
+//! ### Dependency injection - async
+//! // An effect that requires access to a `reqwest::Client`.
+//! ```
+//! let client = reqwest::Client::new();
+//! let effect = once_dependency(&client);
+//! effect.map_output_async(|client|
+//!     client.get("https://www.google.com").send().await.unwrap().text.await.unwrap()
+//! );
+//! let future = effect.resolve(&client);
+//! let output = futures::runtime::block_on(future);
+//! println!("{output}");
+//! ```
+//! ### Module-level access control
+//! // An effect that requires access to an inaccessible application state
+//! ```
+//! #[cfg(Default)]
+//! struct AppState {
+//!     component: component::Component,
+//! }
+//! #[cfg(Default)]
+//! mod component {
+//!     // Note no public setters, so parent modules can't change content...
+//!     pub struct Component {
+//!         content: String
+//!     }
+//!     impl Component {
+//!         fn update_content(&mut self) -> impl Effect<(&mut Self, &reqwest::Client)> {
+//!             // ...but we can provide parent module an Effect that grants the required access.
+//!             once_dependency::<(&mut Self, &Client)>
+//!                 .map_output_async(|(this, client)|
+//!                     let output = client.get("https://www.google.com").send().await.unwrap().text.await.unwrap();
+//!                     this.content = output;
+//!                 )
+//!         }
+//!         fn get_content(&self) -> &str {
+//!             self.content
+//!         }
+//!     }
+//! }
+//! let mut state = AppState::default();
+//! let client = reqwest::Client::new();
+//! let effect = state.component.update_content();
+//! let future = effect.resolve((&mut state.component, &client));
+//! futures::runtime::block_on(future);
+//! // content should contain the body of Google.com
+//! println!("{}", state.component.get_content());
+//! ```
 
 pub mod adapters;
 
 pub trait Effect<D> {
     type Output;
-    fn into_stream(self, dependency: D) -> impl Stream<Item = Self::Output>;
+    fn resolve(self, dependency: D) -> Self::Output;
 }
 
 /// Basic Effect returning the contained value once.
@@ -48,8 +113,8 @@ struct Once<T>(T);
 
 impl<T> Effect<()> for Once<T> {
     type Output = T;
-    fn into_stream(self, dependency: ()) -> impl Stream<Item = Self::Output> {
-        stream::once(async { self.0 })
+    fn resolve(self, _dependency: ()) -> T {
+        self.0
     }
 }
 
@@ -64,17 +129,16 @@ fn once_async<T, F: AsyncFn() -> T>(f: F) -> OnceAsync<F> {
     OnceAsync(f)
 }
 
-impl<F, T> Effect<()> for OnceAsync<F>
+impl<F, Fut, T> Effect<()> for OnceAsync<F>
 where
     // AsyncFnOnce is required here, is I'm unsure how to declare an AsyncFn that does not return a value containing a lifetime.
-    F: AsyncFnOnce() -> T,
-    F: 'static,
+    F: Fn() -> Fut,
+    Fut: Future<Output = T>,
+    // F: 'static,
 {
-    type Output = T;
-    fn into_stream(self, _dependency: ()) -> impl Stream<Item = Self::Output> {
-        let OnceAsync(f) = self;
-        let f = f();
-        stream::once(f)
+    type Output = Fut;
+    fn resolve(self, _dependency: ()) -> Self::Output {
+        self.0()
     }
 }
 
@@ -83,28 +147,14 @@ where
 /// let x = OnceDependency;
 /// assert_eq!(x.into_stream("Hello, world!").next().await, "Hello, world!");
 /// ```
-struct OnceDependency;
+struct OnceDependency<D>(core::marker::PhantomData<D>);
 
-impl<D> Effect<D> for OnceDependency {
-    type Output = D;
-    fn into_stream(self, dependency: D) -> impl Stream<Item = Self::Output> {
-        stream::once(async { dependency })
-    }
+fn once_dependency<D>() -> OnceDependency<D> {
+    OnceDependency(core::marker::PhantomData)
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::Effect;
-
-    #[test]
-    fn test() {
-        struct FetchesNetworkEffect;
-        impl Effect<()> for FetchesNetworkEffect {
-            type Output = ();
-            fn into_stream(self, dependency: ()) -> impl futures::Stream<Item = Self::Output> {
-                futures::stream::once(async { |x| x + 1 })
-            }
-        }
-        let x = FetchesNetworkEffect;
+impl<D> Effect<D> for OnceDependency<D> {
+    type Output = D;
+    fn resolve(self, dependency: D) -> Self::Output {
+        dependency
     }
 }
